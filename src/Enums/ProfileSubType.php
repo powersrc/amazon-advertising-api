91946548a993f583fae253cfa0cf79f4fda2767e
Jason Hebert <jason.hebert@powersrc.net>
2022-01-17 15:23:59 -0600

    Add additional properties to ProfileAccountInfo model
    Add `subType` and `validPaymentMethod` properties.

---
+++ src/Enums/ProfileSubType.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PowerSrc\AmazonAdvertisingApi\Enums;
+
+/**
+ * @method static ProfileSubType KDP_AUTHOR()
+ * @method static ProfileSubType AMAZON_ATTRIBUTION()
+ */
+class ProfileSubType extends Enum
+{
+    public const KDP_AUTHOR         = 'KDP_AUTHOR';
+    public const AMAZON_ATTRIBUTION = 'AMAZON_ATTRIBUTION';
+}
