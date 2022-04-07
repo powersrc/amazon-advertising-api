346fe8877f362ac9f9e5570054018134787416d7
Jason Hebert <jason.hebert@powersrc.net>
2022-04-07 09:28:28 -0500

    Add additional country and currency codes

--- src/Enums/CountryCode.php
+++ src/Enums/CountryCode.php
@@ -7,21 +7,39 @@ namespace PowerSrc\AmazonAdvertisingApi\Enums;
 /**
  * @method static CountryCode US()
  * @method static CountryCode CA()
+ * @method static CountryCode MX()
  * @method static CountryCode UK()
  * @method static CountryCode DE()
  * @method static CountryCode FR()
  * @method static CountryCode ES()
  * @method static CountryCode IT()
  * @method static CountryCode JP()
+ * @method static CountryCode BR()
+ * @method static CountryCode NL()
+ * @method static CountryCode AU()
+ * @method static CountryCode AE()
+ * @method static CountryCode SG()
+ * @method static CountryCode SE()
+ * @method static CountryCode PL()
+ * @method static CountryCode TR()
  */
 class CountryCode extends Enum
 {
     public const US = 'US';
     public const CA = 'CA';
+    public const MX = 'MX';
     public const UK = 'UK';
     public const DE = 'DE';
     public const FR = 'FR';
     public const ES = 'ES';
     public const IT = 'IT';
     public const JP = 'JP';
+    public const BR = 'BR';
+    public const NL = 'NL';
+    public const AU = 'AU';
+    public const AE = 'AE';
+    public const SG = 'SG';
+    public const SE = 'SE';
+    public const PL = 'PL';
+    public const TR = 'TR';
 }
--- src/Enums/CurrencyCode.php
+++ src/Enums/CurrencyCode.php
@@ -10,6 +10,14 @@ namespace PowerSrc\AmazonAdvertisingApi\Enums;
  * @method static CurrencyCode GBP()
  * @method static CurrencyCode EUR()
  * @method static CurrencyCode JPY()
+ * @method static CurrencyCode MXN()
+ * @method static CurrencyCode BRL()
+ * @method static CurrencyCode AUD()
+ * @method static CurrencyCode AED()
+ * @method static CurrencyCode SEK()
+ * @method static CurrencyCode PLN()
+ * @method static CurrencyCode SGD()
+ * @method static CurrencyCode TRY()
  */
 class CurrencyCode extends Enum
 {
@@ -18,4 +26,12 @@ class CurrencyCode extends Enum
     public const GBP = 'GBP';
     public const EUR = 'EUR';
     public const JPY = 'JPY';
+    public const MXN = 'MXN';
+    public const BRL = 'BRL';
+    public const AUD = 'AUD';
+    public const AED = 'AED';
+    public const SEK = 'SEK';
+    public const PLN = 'PLN';
+    public const SGD = 'SGD';
+    public const TRY = 'TRY';
 }
